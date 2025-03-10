99a19eca22b798a10785aa5b5b0300a663bd8b1b
Larson Davis <larsonjdavis@gmail.com>
2025-03-10 06:22:14 -0600

    Including the base main file for the project with comments explaining.
    I am first including comments with a description for the project and why I am building it, read there-
    
    This first version of 'main' simply includes a "Hello World" string.
    Proud enough to say that I was able to write this without any help!
    XDXDXD

---
+++ text_editor_main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <termios.h>
+
+/*
+    This is my first actual C project. I am following along from a tutorial on a website which can be found at link -
+    https://viewsourcecode.org/snaptoken/kilo/
+
+    This is a Command Line Interface text editor.
+    I am building this using Visual Studio Code and WSL (Windows Subsystem for Linux), and, like in the tutorial, I am using GCC and Make.
+    My purpose is to learn more, not only about C, but about things like managing state, memory managment, etc.
+    
+    
+    I tried to follow this a few years ago, but did not understand enough fundamentally about programming in general, about the C language,
+    or about other neccessary concepts, such as compiling.
+*/
+
+
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
