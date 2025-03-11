a73898285de3e1b54f6b329a6600977a6ebfb193
Larson Davis <larsonjdavis@gmail.com>
2025-03-11 09:57:21 -0600

    Completed .gitignore and initial main statements along with comments
    Completed the .gitignore to ignore all of my associations with Visual Studio Code, Makefiles, etc.
    I added comments preceded with |#| where # will represent the order in which code is added and explained.
    Replaced printing hello world with a while loop reading keyboard input.

--- text_editor_main.c
+++ text_editor_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <termios.h>
+#include <unistd.h>
 
 /*
     This is my first actual C project. I am following along from a tutorial on a website which can be found at link -
@@ -18,8 +19,25 @@
     It may not be necessary, but I included a Makefile and added quite a bit to make it more readable, maintainable, and expandable.
     I've included a clean 'rule' in the Makefile, which will remove the compiled file by running 'make clean'.
 */
+/*
+    At the beginning of each comment below, it will begin with a number within a pipe, such as |1| which represents the step at which this piece of code was written.
+    There are many steps where I will be going back and forth to create this project. Helps me - Helps you.
+*/
 
 int main() {
-    printf("Hello World!\n");
+    //|1|-Creating a variable that holds input from the keyboard input stream and name it 'c'
+    //|1|-char's hold 1 byte of memory, or 8 bits, so this is enough to hold a single character
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    /* 
+    |1|-Now, we need to create a loop that continually reads for the input from the keyboard stream and saves it to the variable 'c'.
+
+    We will do this with the read() function, reading from STDIN_FILENO which is the keyboard input stream. The second argument is the buffer where we will store the input,
+    which is &c (address of c), and the third argument is the number of bytes to read, which is 1 byte. Because we are doing this in a 'while' loop, the condition is that the read() function is equal to 1 byte.
+    If the read() function is not equal to 1 byte, then the loop will break and the program will end.
+
+    We also added a second conditional to the while loop, which is that the character is NOT equal to 'q'. So, if the user types a word or the letter q and hit's enter, the program will only read up until the 'q'.
+    Anything typed after the letter q will overflow into terminal. - |1|
+    */
     return 0;
 }
