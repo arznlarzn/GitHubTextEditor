1cefe92e1b6163cb4ad2e511cad9414c897c0142
Larson Davis <larsonjdavis@gmail.com>
2025-03-12 14:59:21 -0600

    added exitRawMode, created a variable to store original attributes for terminal, and added library for a function atexit()
    There are tons of comments that are probably going to be more confusing than helpful to someone else reading this.
    It should be apparent that my comments are really for myself, to understand on a deeper level what I am changing and why.
    It's getting a little messy because of these comments, so if you are trying to understand, pay close attention to the number in pipes at the beginning of each comment. Again, this tells you what step I am on.
    All of the added comments and changed code in this step will be connected with |3|.

--- text_editor_main.c
+++ text_editor_main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -24,12 +25,27 @@
     There are many steps where I will be going back and forth to create this project. Helps me - Helps you.
 */
 
-//|2| creating a function that will begin to enable raw mode. Many steps following will be disabling flags to create a truly 'raw' mode.
+//|3| Creating a struct that holds the original termios attributes, before we change them- starting with the prototype.
+//Prototypes//
+struct termios orig_termios;
+
+//|3| Creating a disableRawMode function
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+//|2| Creating a function that will begin to enable raw mode. Many steps following will be disabling flags to create a truly 'raw' mode.
 void enableRawMode() {
-    struct termios raw;
+    //|3| getting original termios attributes and storing them at the memory address of 'orig_termios'
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    //|3| using atexit() from the stdlib.h library and calling disableRawMode() which will set termios back to its original attributes
+    atexit(disableRawMode);
     //|2| struct termios is a struct variable that holds terminal attributes. Comes in the termios.h library.
-    tcgetattr(STDIN_FILENO, &raw);
+    //struct termios raw;
+    //|3|changing struct termios raw; to- this way it takes the original attributes, and gives us a struct at which we can begin to make changes.
+    struct termios raw = orig_termios;
     //|2| tcgetattr() gets the Current Terminal attributes- getting the attributes of the file descriptor STDIN_FILENO (keyboard input stream), and we are storing them at the address of the variable 'raw'.
+    //tcgetattr(STDIN_FILENO, &raw);   |3| we are moving this line to the top of the function and changing it to-
     raw.c_lflag &= ~(ECHO);
     //|2| c_lflag is a flag that Controls the Local behavior of the terminal. We are using the bitwise AND operator to turn off ECHO, a bitmask within c_lflag, a bitfield.
         //&= is the bitwise AND assignment operator. If we just had &= ECHO, we would turn off ALL other flags except for ECHO. We want to keep the current value, except we turn OFF ECHO by adding the bitwise NOT operator ~.
